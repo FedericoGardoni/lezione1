e0e599b3e80b7f68c5fadfae202e6f17451d3398
FedericoGardoni <32303336+FedericoGardoni@users.noreply.github.com>
2017-09-27 12:49:58 +0200

    First Work

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas (500,500);
+}
+
+function draw() {
+  /*
+  background("#f7eda0");
+  strokeWeight(2);
+  stroke(40,40,40);
+  fill(240,240,240);
+// line(350,50,400,200); 
+ ellipse(350,150,100,200); //occhio d
+ ellipse(150,150,100,200); //occhio s
+ fill(255,0,0);
+ noStroke();
+ ellipse(250,315,50,50); //naso
+ fill(30,30,30);
+ ellipse(350,210,70,80); //pup d
+ ellipse(150,210,70,80); //pup s
+ fill(210,20,20,240);
+ rect(100,400,300,30);
+ fill(190,80,80);
+ rect(100,430,300,25);
+ */
+ 
+// background(220,220,220);
+ ellipse(pmouseX,pmouseY,mouseX,mouseY);
+}
